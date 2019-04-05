0c70cd0cceb9008ab2e514f3e8a0a6a9e55f75e0
tactnician <tactnician@gmail.com>
2019-04-05 15:48:29 -0400

    completed mvp w/ some stretch

--- router/router-helper.js
+++ router/router-helper.js
@@ -1,10 +1,44 @@
 const db = require('../data/dbConfig');
 
 module.exports = {
-    get
-    // getById
+    get,
+    getById,
+    insert,
+    getProjectActions,
+    insertAction
 }
 
+//Projects
 function get() {
     return db('projects')
+};
+
+function getById(id) {
+    return db('projects')
+    .where({id})
+    .first()
+};
+
+function insert(project) {
+    return db('projects')
+    .insert(project)
+    .then(([id]) => this.get(id));
+};
+
+
+//Actions
+function getProjectActions(projectId) {
+    return db('actions')
+    .where('project_id', projectId)
+    .then(actions => actions.map(action => {
+        return {
+            ...action
+        };
+    }));
+}
+
+function insertAction(action) {
+    return db('actions')
+        .insert(action)
+        .then(([id]) => this.get(id));
 }
--- router/router.js
+++ router/router.js
@@ -2,6 +2,8 @@ const router = require('express').Router();
 
 const Projects = require('./router-helper');
 
+
+//Projects
 //get()
 router.get('/', async (req, res) => {
     try {
@@ -25,6 +27,39 @@ router.get('/:id', async (req, res) => {
     } catch (error) {
         console.log(`error in get method ${error}`);
         res.status(500).json({message: `error getting resources: ${error}`});
+    }
+});
+
+//insert()
+router.post('/', async (req, res) => {
+    try {
+        const newResource = await Projects.insert(req.body)
+        res.status(200).json(newResource)
+    } catch (error) {
+        res.status(500).json(`error creating resource ${error}`)
+    }
+});
+
+//get Actions by ID 
+router.get('/:id/actions', async (req, res) => {
+    try {
+        // const {id} =  await req.params.id;
+        const resourceArr = await Projects.getProjectActions(req.params.id);
+        console.log(" resource Array :" , resourceArr);
+        res.status(200).json(resourceArr);
+    } catch (error) {
+        console.log(`error in get method ${error}`);
+        res.status(500).json({message: `error getting resources: ${error}`});
+    }
+});
+
+//insertAction()
+router.post('/actions', async (req, res) => {
+    try {
+        const newResource = await Projects.insertAction(req.body)
+        res.status(200).json(newResource);
+    } catch (error) {
+        res.status(500).json(`error creating resource ${error}`)
     }
 });
 
